fb0a695c6a18fe404241f5b06864de0b2c7f597d
jenkins <yuvalaws12@gmail.com>
2023-08-28 14:06:21 +0300

    deploy.sh added

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,23 @@
 
 VERSION=$1
 COMMIT_HASH=$2
-docker build -t "bestpracticeimg:$VERSION" .
+# Error handling must get 2 inputs from user
+if [ $# -ne 2 ]
+then
+    echo "USAGE: $0 <version> <commit-hash>"
+    exit 1
+fi
 
-git tag "v$VERSION" $COMMIT_HASH
+# Tag & Build thin.dockerfile
+docker build -t "bestpracticeimg:$VERSION"  -f thin.dockerfile .
 
-git push origin "v$VERSION" 
+# Error handling: Checks if build is completed succefuly
+if [ $? -eq 0 ]
+then
+    # Tag & push commit hash
+    git tag "v$VERSION" $COMMIT_HASH
+    git push -u origin "v$VERSION" 
+else
+    echo "Error: Build failed, Fix dockerfile and try again"
+fi
 
